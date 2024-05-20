8e40e5c362d4684a4d2ec3158be1b29aba93aa8b
Tabbomat <2329213+Tabbomat@users.noreply.github.com>
2024-05-20 10:11:19 +0200

    get newest changes from gist

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@
 # sudo ip route del default
 
 # install packages
-sudo apt update &&
+sudo apt update --allow-insecure-repositories
 sudo apt install -y cmake gcc build-essential libx11-dev zlib1g-dev libjpeg62-turbo-dev libpixman-1-dev libfltk1.3-dev libgnutls28-dev libpam0g-dev gettext libxi-dev libxrender-dev netcat &&
 
 # install vncviewer
@@ -19,10 +19,8 @@ cd ~ && rm -rf vnc &&
 # setup xinitrc
 echo 'until nc -z 192.168.214.1 5900
 do
-    echo Waiting for VNC
     sleep 1
 done
-
 exec /usr/local/bin/vncviewer -FullScreen -geometry 1920x1080+0+0 -MenuKey Pause −ReconnectOnError=0 192.168.214.1' > ~/.xinitrc &&
 
 # setup auto startx
@@ -40,16 +38,17 @@ rm /tmp/login_override.txt &&
 sudo systemctl enable getty@tty1.service &&
 
 # remove rule to renew password after 90 days
-sudo sed -i '/^PASS_MAX_DAYS\s+90/d; /^PASS_WARN_AGE\s+80/d' /etc/login.defs
+sudo sed -i -r '/^PASS_MAX_DAYS\s+90/d; /^PASS_WARN_AGE\s+80/d' /etc/login.defs
+sudo passwd -x -1 auduser
 
 # handle display timeout
-if ! grep -q 'Section "ServerFlags"' /usr/share/X11/xorg.conf.d/10-quirks.conf; then
-    sudo echo 'Section "ServerFlags"
+if ! sudo grep -q 'Section "ServerFlags"' /usr/share/X11/xorg.conf.d/10-quirks.conf; then
+    echo 'Section "ServerFlags"
     Option "BlankTime" "240"
     Option "StandbyTime" "240"
     Option "SuspendTime" "240"
     Option "OffTime" "240"
-EndSection' >> /usr/share/X11/xorg.conf.d/10-quirks.conf
+EndSection' | sudo tee -a /usr/share/X11/xorg.conf.d/10-quirks.conf >/dev/null
 fi
 
 echo "Bitte Internetkabel aus eno1 entfernen und eno2 mit der Steuerung verbinden, danach ENTER drücken"
